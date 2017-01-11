8e194bec1b92b979aee34e458fa16847ed62005d
Nicolas Duchemin <duchemin.nicolas@gmail.com>
2017-01-11 04:18:06 +0000

    Added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Contact from validations
     validates :name, presence: true 
     validates :email, presence: true
     validates :comments, presence: true
